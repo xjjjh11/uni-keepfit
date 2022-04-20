be6ed9ac05a09ea3b325a000a00457af800b85ba
xjjjh11 <982587532@qq.com>
2022-04-20 13:32:52 +0800

    完成了 tabBar 的开发

---
+++ pages/store/store.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    store
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
